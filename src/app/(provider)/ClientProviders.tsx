76a07c16d3ea4db9ef9c0531642a28ed5c4b0eeb
Torres Leo <leo.torres@etu.umontpellier.fr>
2024-09-23 10:12:06 +0200

    feat(): provider complete

--- src/app/(provider)/ClientProviders.tsx
+++ src/app/(provider)/ClientProviders.tsx
@@ -2,20 +2,20 @@
 "use client";
 
 import React from "react";
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { AuthProvider } from '@/context/AuthContext';
-import { Provider } from "@/app/(provider)/provider";
+import {QueryClient, QueryClientProvider} from 'react-query';
+import {Provider} from "@/app/(provider)/provider";
+import {AuthProvider} from "@/context/AuthContext";
 
 const queryClient = new QueryClient();
 
-export default function ClientProviders({ children }: { children: React.ReactNode }) {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <Provider>
-          {children}
-        </Provider>
-      </AuthProvider>
-    </QueryClientProvider>
-  );
+export default function ClientProviders({children}: { children: React.ReactNode }) {
+    return (
+        <QueryClientProvider client={queryClient}>
+            <AuthProvider>
+                <Provider>
+                    {children}
+                </Provider>
+            </AuthProvider>
+        </QueryClientProvider>
+    );
 }
