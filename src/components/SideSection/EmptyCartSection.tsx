336fd1de53eeabb2ccf730ceca1251604563f67b
Torres Leo <leo.torres@etu.umontpellier.fr>
2024-09-23 10:04:45 +0200

    feat(): section pour cart vide

---
+++ src/components/SideSection/EmptyCartSection.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Image from 'next/image';
+import empty_card from '@public/images/common/empty-card.svg';
+
+interface EmptyCartSectionProps {
+    closeSidebar: () => void;
+}
+
+const EmptyCartSection: React.FC<EmptyCartSectionProps> = ({ closeSidebar }) => {
+    return (
+        <div className="sideBar_container flex flex-col h-[80vh]">
+            <div className="sideBar_empty-card-section flex flex-col items-center justify-center w-full h-full">
+                <Image  src={empty_card} alt="empty card" className='mb-4 w-[200px] h-[200px]' />
+                <h3 className='font-semibold text-xl mb-4 text-[#666666]'>
+                    Panier vide
+                </h3>
+                <p className='text-center text-[#666666]'>
+                    Ajoutez des articles au panier, puis revenez voir par ici !
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default EmptyCartSection;
