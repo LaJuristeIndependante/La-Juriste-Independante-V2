b834c6e13bae4cfffaa3682e9255d5ba50dab603
Torres Leo <leo.torres@etu.umontpellier.fr>
2024-09-24 16:54:29 +0200

    feat(): update page principale

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import ProductSection from "@lib/ProductLib/component/ProductSection";
 import NewsLetterSection from "@lib/NewsLetterLib/component/NewsLetterSection";
 import TestimonialsSection from "@lib/testimonialLib/component/TestimonialsSection";
+import FirstSection from "@/components/home/FirstSection";
+import SecondSection from "@/components/home/SecondSection";
 
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between">
-            <ProductSection/>
+            <FirstSection/>
+            <SecondSection/>
+            {/*<ProductSection/>*/}
             <TestimonialsSection/>
         </main>
     );
