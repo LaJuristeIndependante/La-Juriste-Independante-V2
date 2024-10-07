de2f8003bfc6aed32ec854e93fbe664a40c04f7a
Torres Leo <leo.torres@etu.umontpellier.fr>
2024-10-07 15:31:10 +0200

    feat(): globals transfert design font

--- src/app/(auth)/admin/product/page.tsx
+++ src/app/(auth)/admin/product/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import dynamic from "next/dynamic";
+import BubbleBackground from "@/components/utils/décors/BubbleBackground";
 
 const ProductForm = dynamic(() => import("@lib/UserLib/component/admin/product/AdminProductSection"), {
     ssr: false, // Désactive le rendu côté serveur
@@ -8,10 +9,12 @@ const ProductForm = dynamic(() => import("@lib/UserLib/component/admin/product/A
 
 const AdminProductPage: React.FC = () => {
     return (
-        <main className="relative flex items-center justify-center min-h-screen">
-            <ProductForm/>
+        <main className="relative flex items-center justify-center min-h-screen z-1">
+            <ProductForm />
+            <BubbleBackground size={200} position={"-right-16 -bottom-16"} />
+            <BubbleBackground size={300} position={"-left-16 -bottom-16"} />
         </main>
     );
-}
+};
 
 export default AdminProductPage;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,13 @@
 import type { Config } from "tailwindcss";
+import plugin from 'tailwindcss/plugin';
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./_lib/**/*.{js,ts,jsx,tsx}"
+    "./_lib/**/*.{js,ts,jsx,tsx}",
+    "./public/**/*.{js,ts,jsx,tsx,mdx,ttf,otf}",
   ],
   theme: {
     extend: {
@@ -14,8 +16,23 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        'jost': ['Jost', 'sans-serif'],
+        'island': ['IslandMoments', 'sans-serif'],
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({addUtilities}) {
+      addUtilities({
+        '.font-jost': {
+          fontFamily: 'var(--font-primary)',
+        },
+        '.font-island': {
+          fontFamily: 'var(--font-secondary)',
+        },
+      });
+    }),
+  ],
 };
 export default config;
