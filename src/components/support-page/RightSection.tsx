d33ea7dbd10e5fffc50bf5d47b70b5afd9f6bb9a
Torres Leo <leo.torres@etu.umontpellier.fr>
2024-10-10 15:07:59 +0200

    fix(): contact page fonctionnel

--- src/components/support-page/RightSection.tsx
+++ src/components/support-page/RightSection.tsx
@@ -6,151 +6,152 @@ import user_icon from '@public/images/common/auth-icon.svg';
 import mail_icon from '@public/images/common/mail-icon2.svg';
 import object_icon from '@public/images/common/object-icon.svg';
 import textarea_icon from '@public/images/common/textarea-icon.svg';
-import {sendContactForm} from "@lib/Contact/type/ContactType";
+import {sendContactFormLJI} from "@lib/Contact/type/ContactType";
 import ValidationPopup from "@/components/utils/décors/ValidationPopUp";
 
 function RightSection() {
-  const [isMobile, setIsMobile] = useState(false);
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    message: "",
-    objet: "",
-    consent: false,
-  });
-  const [status, setStatus] = useState<string>("");
-  const [showPopup, setShowPopup] = useState(false);
+    const [isMobile, setIsMobile] = useState(false);
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        message: "",
+        objet: "",
+        consent: false,
+    });
+    const [status, setStatus] = useState<string>("");
+    const [showPopup, setShowPopup] = useState(false);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 803);
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 803);
+        };
+
+        handleResize();
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const {name, value, type} = e.target;
+        if (type === "checkbox") {
+            const {checked} = e.target as HTMLInputElement;
+            setFormData((prevData) => ({
+                ...prevData,
+                [name]: checked,
+            }));
+            if (name === "consent" && checked) {
+                setShowPopup(true);
+            }
+        } else {
+            setFormData((prevData) => ({
+                ...prevData,
+                [name]: value,
+            }));
+        }
     };
 
-    handleResize();
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault();
+        setStatus("Sending");
+        try {
+            const data = await sendContactFormLJI(formData);
+            if (data.success) {
+                setStatus("Message envoyé !");
+                setFormData({name: "", email: "", message: "", objet: "", consent: false});
+            } else {
+                setStatus("Problème lors de l'envoi du mail");
+            }
+        } catch (error) {
+            setStatus("Erreur lors de l'envoi du formulaire");
+        }
     };
-  }, []);
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const { name, value, type } = e.target;
-    if (type === "checkbox") {
-      const { checked } = e.target as HTMLInputElement;
-      setFormData((prevData) => ({
-        ...prevData,
-        [name]: checked,
-      }));
-      if (name === "consent" && checked) {
-        setShowPopup(true);
-      }
-    } else {
-      setFormData((prevData) => ({
-        ...prevData,
-        [name]: value,
-      }));
-    }
-  };
-
-  const handleSubmit = async (e: FormEvent) => {
-    e.preventDefault();
-    setStatus("Sending");
-    try {
-      const data = await sendContactForm(formData);
-      if (data.success) {
-        setStatus("Message envoyé !");
-        setFormData({ name: "", email: "", message: "", objet: "", consent: false });
-      } else {
-        setStatus("Problème lors de l'envoi du mail");
-      }
-    } catch (error) {
-      setStatus("Erreur lors de l'envoi du formulaire");
-    }
-  };
-
-  return (
-    <div className={`border-2 border-black max-w-[600px] flex flex-col items-start justify-center p-4 h-auto md:h-[500px] ${isMobile ? 'text-sm w-11/12' : 'text-base w-full'}`}>
-      <form className="w-full">
-        <h3 className={`font-bold mt-6 mb-8 w-full ${isMobile ? 'text-3xl' : 'text-5xl'}`}>
-          Contactez-nous
-        </h3>
-        <div onSubmit={handleSubmit}
-              className="flex flex-col md:flex-row md:items-center md:justify-between w-full space-y-4 md:space-y-0">
-          <div className='w-full md:w-[48%] flex items-center'>
-            <InputAnimation
-                label='Nom'
-                icon={user_icon}
-                utility='text'
-                type="text"
-                name="name"
-                placeholder="Entrez votre nom prénom"
-                value={formData.name}
-                onChange={handleChange}
-            />
-          </div>
-          <div className='w-full md:w-[48%] flex items-center'>
-            <InputAnimation
-                label='Adresse mail'
-                icon={mail_icon}
-                utility='text'
-                type="email"
-                name="email"
-                placeholder="Entrez votre email"
-                value={formData.email}
-                onChange={handleChange}
-            />
-          </div>
+    return (
+        <div
+            className={`border-2 border-black max-w-[600px] flex flex-col items-start justify-center p-4 h-auto md:h-[500px] ${isMobile ? 'text-sm w-11/12' : 'text-base w-full'}`}>
+            <h3 className={`font-bold mt-6 mb-8 w-full ${isMobile ? 'text-3xl' : 'text-5xl'}`}>
+                Contactez-nous
+            </h3>
+            <form onSubmit={handleSubmit} className="w-full">
+                <div
+                    className="flex flex-col md:flex-row md:items-center md:justify-between w-full space-y-4 md:space-y-0">
+                    <div className='w-full md:w-[48%] flex items-center'>
+                        <InputAnimation
+                            label='Nom'
+                            icon={user_icon}
+                            utility='text'
+                            type="text"
+                            name="name"
+                            placeholder="Entrez votre nom prénom"
+                            value={formData.name}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className='w-full md:w-[48%] flex items-center'>
+                        <InputAnimation
+                            label='Adresse mail'
+                            icon={mail_icon}
+                            utility='text'
+                            type="email"
+                            name="email"
+                            placeholder="Entrez votre email"
+                            value={formData.email}
+                            onChange={handleChange}
+                        />
+                    </div>
+                </div>
+                <div className="w-full mb-6">
+                    <InputAnimation
+                        label='Objet'
+                        icon={object_icon}
+                        utility='text'
+                        type='text'
+                        name={'objet'}
+                        value={formData.objet}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className='w-full h-40 bg-[#F5F5F5] flex items-center mb-2 border-gray-200 border rounded-md'>
+                    <textarea
+                        className='w-11/12 h-full p-4 bg-[#F5F5F5] resize-none outline-none'
+                        name="message"
+                        placeholder="Entrez votre message"
+                        value={formData.message}
+                        onChange={handleChange}
+                        required
+                    />
+                    <span className='w-1/12 h-full flex items-center justify-center bg-[#F5F5F5]'>
+                        <Image src={textarea_icon} alt='textarea_icon' className='w-6 h-6'/>
+                    </span>
+                </div>
+                <div className={"mb-5"}>
+                    <input
+                        type="checkbox"
+                        name="consent"
+                        checked={formData.consent}
+                        onChange={handleChange}
+                        className="mr-2"
+                        required
+                    />
+                    <label className="text-black">Consentement*</label>
+                </div>
+                <button type="submit"
+                        className='w-full bg-[#DD2A27] text-white focus:outline-none py-2 rounded-md hover:bg-[#FF4D4D] mb-4'>
+                    Envoyer
+                </button>
+            </form>
+            {showPopup && (
+                <ValidationPopup
+                    title="Le consentement lors d'un envoi"
+                    text="Lorem ipsum dolor sit amet. Et velit praesentium quo voluptas impedit vel nulla quam! Qui perferendis repudiandae cum quam sequi et sunt quasi et rerum iusto ex eius modi. Qui laboriosam quasi rem corrupti sunt vel amet aliquam et quidem iure est omnis laboriosam. Et odio sint rem esse doloribus aut internos autem cum placeat corporis!"
+                    onClose={() => setShowPopup(false)}
+                />
+            )}
         </div>
-        <div className="w-full mb-6">
-          <InputAnimation
-              label='Objet'
-              icon={object_icon}
-              utility='text'
-              type='text'
-              name={'objet'}
-              value={formData.objet}
-              onChange={handleChange}
-          />
-        </div>
-        <div className='w-full h-40 bg-[#F5F5F5] flex items-center mb-2 border-gray-200 border rounded-md'>
-          <textarea
-              className='w-11/12 h-full p-4 bg-[#F5F5F5] resize-none outline-none'
-              name="message"
-              placeholder="Entrez votre message"
-              value={formData.message}
-              onChange={handleChange}
-              required
-          />
-
-          <span className='w-1/12 h-full flex items-center justify-center bg-[#F5F5F5]'>
-              <Image src={textarea_icon} alt='textarea_icon' className='w-6 h-6'/>
-          </span>
-        </div>
-        <div className={"mb-5"}>
-            <input
-                type="checkbox"
-                name="consent"
-                checked={formData.consent}
-                onChange={handleChange}
-                className="mr-2"
-                required
-            />
-            <label className="text-black">Consentement*</label>
-        </div>
-        <button className='w-full bg-[#DD2A27] text-white focus:outline-none py-2 rounded-md hover:bg-[#FF4D4D] mb-4'>
-          Envoyer
-        </button>
-      </form>
-      {showPopup && (
-          <ValidationPopup
-              title="Le consentement lors d'un envoi"
-              text="Lorem ipsum dolor sit amet. Et velit praesentium quo voluptas impedit vel nulla quam! Qui perferendis repudiandae cum quam sequi et sunt quasi et rerum iusto ex eius modi. Qui laboriosam quasi rem corrupti sunt vel amet aliquam et quidem iure est omnis laboriosam. Et odio sint rem esse doloribus aut internos autem cum placeat corporis!"
-              onClose={() => setShowPopup(false)}
-          />
-      )}
-    </div>
-  );
+    );
 }
 
 export default RightSection;
