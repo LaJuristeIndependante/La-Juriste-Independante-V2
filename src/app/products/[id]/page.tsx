d6ebb62344c5aea076841097f32ae59e8d9ad6ad
jean-mulet <leo0609leo@gmail.com>
2024-11-19 16:25:47 +0100

    feat(): bouton paiement

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,29 +1,33 @@
 "use client";
+
 import Image from 'next/image';
-import React, { useState, useEffect } from 'react';
-import { useSession } from "next-auth/react";
-import { notFound, useRouter } from "next/navigation";
-import { ProductDetail, ProductPageProps } from "@/../_lib/ProductLib/type/Product";
-import { fetchProductById } from "@/../_lib/ProductLib/service/produit";
-import { createOrder } from "@/../_lib/OrderLib/service/orders";
-import { FaShoppingCart } from "react-icons/fa";
-import { useMediaQuery } from "react-responsive";
-import { addToCart } from "@/../_lib/CartLib/service/cart";
+import React, {useState, useEffect} from 'react';
+import {useSession} from "next-auth/react";
+import {notFound, useRouter} from "next/navigation";
+import {ProductDetail, ProductPageProps} from "@/../_lib/ProductLib/type/Product";
+import {fetchProductById, fetchProductsForAdmin} from "@/../_lib/ProductLib/service/produit";
+import {createOrder} from "@/../_lib/OrderLib/service/orders";
+import {FaShoppingCart} from "react-icons/fa";
+import {useMediaQuery} from "react-responsive";
+import {addToCart} from "@/../_lib/CartLib/service/cart";
 import left_arrow_icon from '@public/images/common/left-arrow-icon2.svg';
 import arrow_right_icon from '@public/images/common/arrow-right-icon.svg';
 import card_icon from '@public/images/common/cart-icon.svg';
 import BubbleDecoration from "@/../_lib/ProductLib/component/BubbleDecoration";
 import BackgroundBubbles from "@/components/utils/décors/BubbleBackground";
+import {Profession} from "@lib/ProfessionLib/type/Profession";
+import {getAllProfessions} from "@lib/ProfessionLib/service/professionService";
 
-export default function ProductPage({ params }: ProductPageProps) {
+export default function ProductPage({params}: ProductPageProps) {
     const [product, setProduct] = useState<ProductDetail>()
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<boolean>(false);
     const [message, setMessage] = useState<string | null>(null);
     const [isSuccess, setIsSuccess] = useState<boolean | null>(null);
-    const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
-    const { data: session } = useSession();
+    const isMobile = useMediaQuery({query: '(max-width: 768px)'});
+    const {data: session} = useSession();
     const router = useRouter();
+    const [professions, setProfessions] = useState<Profession[]>([]);
 
     useEffect(() => {
         const loadProduct = async () => {
@@ -37,7 +41,17 @@ export default function ProductPage({ params }: ProductPageProps) {
             setLoading(false);
         };
 
+        const loadProfessions = async () => {
+            try {
+                const data = await getAllProfessions();
+                setProfessions(data);
+            } catch (error) {
+                console.error("Erreur lors de la récupération des professions", error);
+            }
+        };
+
         loadProduct();
+        loadProfessions();
     }, [params.id]);
 
     const handleCart = async () => {
@@ -46,7 +60,7 @@ export default function ProductPage({ params }: ProductPageProps) {
         try {
             // Utilisateur connecté
             if (session?.user) {
-                await addToCart({ userId: session.user.id, productId: product._id, quantity: 1 });
+                await addToCart({userId: session.user.id, productId: product._id, quantity: 1});
             } else {
                 // Utilisateur non connecté - Stocker le panier dans le localStorage
                 const cartItems = JSON.parse(localStorage.getItem('cart') || '[]');
@@ -119,7 +133,7 @@ export default function ProductPage({ params }: ProductPageProps) {
                 <div className="flex justify-between items-center w-full">
                     <div className="flex items-center justify-start">
                         <button onClick={() => window.history.back()} className='flex items-center ml-6'>
-                            <Image src={left_arrow_icon} alt="left arrow icon" className='w-12 h-12' />
+                            <Image src={left_arrow_icon} alt="left arrow icon" className='w-12 h-12'/>
                         </button>
                         <div className="flex flex-col mt-20 ml-3">
                             <h2 className='text-3xl font-semibold text-center mt-10 mb-10 ml-3'>
@@ -129,24 +143,26 @@ export default function ProductPage({ params }: ProductPageProps) {
                                     <span>{product.name}</span>
                                 )}
                             </h2>
-                            <p className='bg-gray-200 p-2 rounded-lg text-center ml-3 mb-8'>
+                            <p className='text-start ml-3 mb-8'>
                                 {product && (
-                                    <span className='w-full text-center'>
-                                        {product.profession && typeof product.profession === 'string' ? product.profession : ''}
-                                    </span>
+                                    <p className={`bg-gray-200 p-2 rounded-lg max-w-min`}>
+                                        {
+                                            professions.find(prof => prof._id === product.profession)?.name || 'Profession inconnue'
+                                        }
+                                    </p>
                                 )}
                             </p>
                         </div>
                     </div>
-                    <hr className="w-1/4 md:w-[400px] border-[#DA1A32] border-[12px] md:border-l-8 rounded-l-xl border-special-red my-10" />
+                    <hr className="w-1/4 md:w-[400px] border-[#DA1A32] border-[12px] md:border-l-8 rounded-l-xl border-special-red my-10"/>
                 </div>
                 <div className="flex flex-col md:flex-row items-center h-full w-full justify-between">
                     <div className="w-2/3 flex items-center h-full justify-center">
-                        <ModelCart3 product={product} />
+                        <ModelCart3 product={product}/>
                     </div>
-                    <div className="flex flex-col items-center justify-center w-full h-full">
+                    <div className="relative flex flex-col items-center justify-center w-full p-10 space-y-8">
                         {!isMobile ? (
-                            <p className='max-w-[700px] text-lg xs:text-xl md:text-xl xl:text-xl text-justify'>
+                            <p className='text-lg xs:text-xl md:text-xl xl:text-xl text-justify'>
                                 {product.description || 'Description non disponible'}
                             </p>
                         ) : (
@@ -157,26 +173,35 @@ export default function ProductPage({ params }: ProductPageProps) {
                                 </p>
                             </details>
                         )}
-                        
+                        <div className="flex items-center justify-center w-full space-x-5">
+                            <button onClick={handleOrder} className={"flex-1 bg-primary-color rounded-2xl p-4 text-white font-bold text-xl"}>
+                                Acheté
+                            </button>
+                            <button onClick={handleCart} className={"flex-1 border-black rounded-2xl border-2 p-4 font-bold text-xl"}>
+                                panier
+                            </button>
+                        </div>
                     </div>
                 </div>
-                <BackgroundBubbles page='contracts' />
+                {/*<BackgroundBubbles page='contracts'/>*/}
             </div>
         </main>
     );
 }
 
-const ModelCart3 = ({ product }: { product: ProductDetail }) => {
+const ModelCart3 = ({product}: { product: ProductDetail }) => {
 
     return (
         <div className='relative'>
-            <div className="relative flex -z-10 flex-col items-center justify-center w-[300px] p-2 pb-0 bg-[#EAEAEA] rounded-lg cursor-pointer transition shadow-lg hover:shadow-xl overflow-hidden">
+            <div
+                className="relative flex -z-10 flex-col items-center justify-center w-[300px] p-2 pb-0 bg-[#EAEAEA] rounded-lg cursor-pointer transition shadow-lg hover:shadow-xl overflow-hidden">
                 <div className='flex flex-col relative items-center justify-center h-[320px] w-[250px] md:w-[400px]'>
                     <div className={"mt-5"}>
-                        <BubbleDecoration size={70} position="-right-5 -bottom-2" opacity={0.5} />
-                        <BubbleDecoration size={70} position="right-5 -bottom-6" opacity={0.3} />
+                        <BubbleDecoration size={70} position="-right-5 -bottom-2" opacity={0.5}/>
+                        <BubbleDecoration size={70} position="right-5 -bottom-6" opacity={0.3}/>
                     </div>
-                    <div className="flex flex-col border border-gray-200 rounded-lg overflow-hidden h-full w-full md:w-[400px]">
+                    <div
+                        className="flex flex-col border border-gray-200 rounded-lg overflow-hidden h-full w-full md:w-[400px]">
                         <div className="flex items-center justify-center w-full border-1 border-black h-full">
                             <div className="flex-col items-center justify-center w-1/2 h-full mt-10">
                                 <h3 className="text-xl font-bold text-center  mb-[35px]">{product.name}</h3>
@@ -186,7 +211,8 @@ const ModelCart3 = ({ product }: { product: ProductDetail }) => {
 
                 </div>
             </div>
-            <div className={"shadow-xl flex flex-col bg-[#F8F8F8] rounded-full items-center ml-[18%] justify-center mt-[-7%] z-50 w-[190px]"}>
+            <div
+                className={"shadow-xl flex flex-col bg-[#F8F8F8] rounded-full items-center ml-[18%] justify-center mt-[-7%] z-50 w-[190px]"}>
                 <p className='font-bold text-2xl p-2 text-center'>
                     {product.price} €
                 </p>
