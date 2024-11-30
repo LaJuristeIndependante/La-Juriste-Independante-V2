df4c6d1c423ebbd47c30f0c88db10f958ceaa30a
LaJuristeIndependante <lajuristeindependante@gmail.com>
2024-11-30 15:08:46 +0100

    Update TitleSection.tsx

--- src/components/home/utils/TitleSection.tsx
+++ src/components/home/utils/TitleSection.tsx
@@ -6,11 +6,11 @@ const TitleSection: React.FC = () => {
     return (
         <div className='min-h-[350px] flex flex-col justify-center items-center'>
             <h1 className="text-3xl sm:text-4xl md:text-4xl lg:text-4xl xl:text-4xl font-bold text-center mt-20 mb-6">
-                Le droit à <span className={"relative"}>porter de main <Image src={line} alt={"line"} className={"absolute right-0"}/></span>
+                Le droit à <span className={"relative"}>portée de main<Image src={line} alt={"line"} className={"absolute right-0"}/></span>
             </h1>
 
             <p className="text-lg sm:text-base md:text-lg lg:text-xl xl:text-2xl max-w-lg font-light text-center w-full sm:w-1/2">
-                Services juridiques personnalisés pour micro-entrepreneurs : protection et réussite garanties !
+                Services de Templates Juridiques pour micro-entrepreneurs : protection et réussite garanties !
             </p>
         </div>
     );
