283482cbae7e36020d46b76d24a423778090e844
jean-mulet <leo0609leo@gmail.com>
2024-12-01 17:06:15 +0100

    refactor(): fortification de la convertion en centime

--- _lib/StripeLib/convertToSubcurrency.ts
+++ _lib/StripeLib/convertToSubcurrency.ts
@@ -1,5 +1,11 @@
 function convertToSubcurrency(amount: number, factor = 100){
-    return Math.round(amount * factor);
+    const result = Math.round(amount * factor);
+
+    if (result < 50){
+        throw new Error('Le prix doit etre supérieur a 50 centime')
+    }
+
+    return result;
 }
 
 export default convertToSubcurrency;
