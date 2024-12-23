7269d0c0611ca090dc204b5320641196de1d9e68
FILLOUX Florian <florian.filloux@etu.univ-lyon1.fr>
2024-12-23 13:05:19 +0100

    feat(): meilleur layout products/[id]

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,31 +1,31 @@
 "use client";
 
 import Image from 'next/image';
-import React, {useState, useEffect} from 'react';
-import {useSession} from "next-auth/react";
-import {notFound, useRouter} from "next/navigation";
-import {ProductDetail, ProductPageProps} from "@/../_lib/ProductLib/type/Product";
-import {fetchProductById, fetchProductsForAdmin} from "@/../_lib/ProductLib/service/produit";
-import {createOrder} from "@/../_lib/OrderLib/service/orders";
-import {FaShoppingCart} from "react-icons/fa";
-import {useMediaQuery} from "react-responsive";
-import {addToCart} from "@/../_lib/CartLib/service/cart";
+import React, { useState, useEffect } from 'react';
+import { useSession } from "next-auth/react";
+import { notFound, useRouter } from "next/navigation";
+import { ProductDetail, ProductPageProps } from "@/../_lib/ProductLib/type/Product";
+import { fetchProductById, fetchProductsForAdmin } from "@/../_lib/ProductLib/service/produit";
+import { createOrder } from "@/../_lib/OrderLib/service/orders";
+import { FaShoppingCart } from "react-icons/fa";
+import { useMediaQuery } from "react-responsive";
+import { addToCart } from "@/../_lib/CartLib/service/cart";
 import left_arrow_icon from '@public/images/common/left-arrow-icon2.svg';
 import arrow_right_icon from '@public/images/common/arrow-right-icon.svg';
 import card_icon from '@public/images/common/cart-icon.svg';
 import BubbleDecoration from "@/../_lib/ProductLib/component/BubbleDecoration";
 import BackgroundBubbles from "@/components/utils/décors/BubbleBackground";
-import {Profession} from "@lib/ProfessionLib/type/Profession";
-import {getAllProfessions} from "@lib/ProfessionLib/service/professionService";
+import { Profession } from "@lib/ProfessionLib/type/Profession";
+import { getAllProfessions } from "@lib/ProfessionLib/service/professionService";
 
-export default function ProductPage({params}: ProductPageProps) {
+export default function ProductPage({ params }: ProductPageProps) {
     const [product, setProduct] = useState<ProductDetail>()
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<boolean>(false);
     const [message, setMessage] = useState<string | null>(null);
     const [isSuccess, setIsSuccess] = useState<boolean | null>(null);
-    const isMobile = useMediaQuery({query: '(max-width: 768px)'});
-    const {data: session} = useSession();
+    const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
+    const { data: session } = useSession();
     const router = useRouter();
     const [professions, setProfessions] = useState<Profession[]>([]);
 
@@ -60,7 +60,7 @@ export default function ProductPage({params}: ProductPageProps) {
         try {
             // Utilisateur connecté
             if (session?.user) {
-                await addToCart({userId: session.user.id, productId: product._id, quantity: 1});
+                await addToCart({ userId: session.user.id, productId: product._id, quantity: 1 });
             } else {
                 // Utilisateur non connecté - Stocker le panier dans le localStorage
                 const cartItems = JSON.parse(localStorage.getItem('cart') || '[]');
@@ -75,7 +75,7 @@ export default function ProductPage({params}: ProductPageProps) {
                         productId: product._id,
                         name: product.name,
                         price: product.price,
-                        pdfFile : product.pdfFile,
+                        pdfFile: product.pdfFile,
                         quantity: 1,
                     });
                 }
@@ -109,7 +109,7 @@ export default function ProductPage({params}: ProductPageProps) {
                     name: product.name,
                     price: product.price,
                     quantity: 1,
-                    pdfFile : product.pdfFile,
+                    pdfFile: product.pdfFile,
                 }],
                 amount: product.price,
             });
@@ -130,45 +130,52 @@ export default function ProductPage({params}: ProductPageProps) {
     }
 
     return (
-        <main className="relative min-h-screen flex items-start justify-center">
-            <div className="w-full">
+        <main className="relative min-h-screen flex-col flex items-start justify-start">
+            <div className="w-full mt-10">
+                {isMobile && (
+                    <h2 className='text-3xl font-semibold text-center w-[95%] mt-10 mb-5 ml-3'>
+                        {!product ? (
+                            <span>Modèle</span>
+                        ) : (
+                            <span>{product.name}</span>
+                        )}
+                    </h2>
+                )}
                 <div className="flex justify-between items-center w-full">
                     <div className="flex items-center justify-start">
                         <button onClick={() => window.history.back()} className='flex items-center ml-6'>
-                            <Image src={left_arrow_icon} alt="left arrow icon" className='w-12 h-12'/>
+                            <Image src={left_arrow_icon} alt="left arrow icon" className='w-12 h-12' />
                         </button>
-                        <div className="flex flex-col mt-20 ml-3">
-                            <h2 className='text-3xl font-semibold text-center mt-10 mb-10 ml-3'>
-                                {!product ? (
-                                    <span>Modèle</span>
-                                ) : (
-                                    <span>{product.name}</span>
-                                )}
-                            </h2>
-                            <p className='text-start ml-3 mb-8'>
-                                {product && (
-                                    <p className={`bg-gray-200 p-2 rounded-lg max-w-min`}>
-                                        {
-                                            professions.find(prof => prof._id === product.profession)?.name || 'Profession inconnue'
-                                        }
-                                    </p>
-                                )}
-                            </p>
-                        </div>
                     </div>
-                    <hr className="w-1/4 md:w-[400px] border-[#A00C30] border-[12px] md:border-l-8 rounded-l-xl border-special-red my-10"/>
+                    {!isMobile && (
+                        <h2 className='text-3xl font-semibold text-center mt-10 mb-10 ml-3'>
+                            {!product ? (
+                                <span>Modèle</span>
+                            ) : (
+                                <span>{product.name}</span>
+                            )}
+                        </h2>
+                    )}
+                    <hr className="w-1/4 md:w-[400px] border-[#A00C30] border-[12px] md:border-l-8 rounded-l-xl border-special-red my-10" />
                 </div>
-                <div className="flex flex-col md:flex-row items-center h-full w-full justify-between">
+                {product && (
+                    <p className={`bg-[#EAEAEA] p-2 rounded-lg w-[300px] md:w-[250px] mx-auto md:mx-0 text-center md:ml-6`}>
+                        {
+                            professions.find(prof => prof._id === product.profession)?.name || 'Profession inconnue'
+                        }
+                    </p>
+                )}
+                <div className="flex flex-col md:flex-row mt-10 items-center h-full w-full justify-between">
                     <div className="w-2/3 flex items-center h-full justify-center">
-                        <ModelCart3 product={product}/>
+                        <ModelCart3 product={product} />
                     </div>
                     <div className="relative flex flex-col items-center justify-center w-full p-10 space-y-8">
                         {!isMobile ? (
-                            <p className='text-lg xs:text-xl md:text-xl xl:text-xl text-justify'>
+                            <p className='text-lg xs:text-xl md:text-xl xl:text-xl text-justify md:max-w-[600px] p-5'>
                                 {product.description || 'Description non disponible'}
                             </p>
                         ) : (
-                            <details className='mt-20 mb-5'>
+                            <details className='mt-10 mb-5'>
                                 <summary className='text-lg font-bold text-center'>Description</summary>
                                 <p className='text-center'>
                                     {product.description || 'Description non disponible'}
@@ -188,17 +195,16 @@ export default function ProductPage({params}: ProductPageProps) {
                             </div>
                         ) : (
                             // eslint-disable-next-line react/no-unescaped-entities
-                            <p>Pour acheté : créez vous un compte afin de pouvoir recevoir et voir vos document aprés l'achat</p>
+                            <p className='text-center mb-4 md:mb-0'>Pour acheter : créez-vous un compte afin de pouvoir recevoir et voir vos documents après l'achat</p>
                         )}
                     </div>
                 </div>
-                {/*<BackgroundBubbles page='contracts'/>*/}
             </div>
         </main>
     );
 }
 
-const ModelCart3 = ({product}: { product: ProductDetail }) => {
+const ModelCart3 = ({ product }: { product: ProductDetail }) => {
 
     return (
         <div className='relative'>
@@ -206,8 +212,8 @@ const ModelCart3 = ({product}: { product: ProductDetail }) => {
                 className="relative flex -z-10 flex-col items-center justify-center w-[300px] p-2 pb-0 bg-[#EAEAEA] rounded-lg cursor-pointer transition shadow-lg hover:shadow-xl overflow-hidden">
                 <div className='flex flex-col relative items-center justify-center h-[320px] w-[250px] md:w-[400px]'>
                     <div className={"mt-5"}>
-                        <BubbleDecoration size={70} position="-right-5 -bottom-2" opacity={0.5}/>
-                        <BubbleDecoration size={70} position="right-5 -bottom-6" opacity={0.3}/>
+                        <BubbleDecoration size={70} position="-right-5 -bottom-2" opacity={0.5} />
+                        <BubbleDecoration size={70} position="right-5 -bottom-6" opacity={0.3} />
                     </div>
                     <div
                         className="flex flex-col border border-gray-200 rounded-lg overflow-hidden h-full w-full md:w-[400px]">
@@ -229,3 +235,12 @@ const ModelCart3 = ({product}: { product: ProductDetail }) => {
         </div>
     )
 }
+
+
+/*
+              
+                 
+                </div>
+            </div>
+*/
+
