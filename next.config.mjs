e61f6d4cb2368f9142408c00973caebc78e31d33
jean-mulet <leo0609leo@gmail.com>
2025-01-05 14:50:45 +0100

    refactor(): test proxy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/api/products/**',
+                destination: 'https://la-juriste-proxy-ec8f63ca9ca5.herokuapp.com/api/products',
+            },
+        ];
+    },
+};
 
 export default nextConfig;
