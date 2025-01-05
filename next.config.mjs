a120ce3aa26e5b4e523cd27c6365f83935ae6c65
jean-mulet <leo0609leo@gmail.com>
2025-01-05 15:14:00 +0100

    refactor(): test proxy

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,9 @@
 const nextConfig = {
     async rewrites() {
         return [
-            // Route pour `/api/products`
-            {
-                source: '/api/products',
-                destination: 'https://la-juriste-proxy-ec8f63ca9ca5.herokuapp.com/api/products',
-            },
-            // Route pour toutes les sous-routes `/api/products/:path*`
             {
                 source: '/api/products/:path*',
-                destination: 'https://la-juriste-proxy-ec8f63ca9ca5.herokuapp.com/api/products/:path*',
+                destination: 'https://your-proxy.herokuapp.com/api/products/:path*', // Remplacez par l'URL de votre proxy
             },
         ];
     },
