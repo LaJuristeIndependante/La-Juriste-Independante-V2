31a0358d596d81681c3f52d8945e023aba500995
jean-mulet <leo0609leo@gmail.com>
2025-01-05 14:54:04 +0100

    refactor(): test proxy

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,15 @@
 const nextConfig = {
     async rewrites() {
         return [
+            // Route pour `/api/products`
             {
-                source: '/api/products/**',
+                source: '/api/products',
                 destination: 'https://la-juriste-proxy-ec8f63ca9ca5.herokuapp.com/api/products',
+            },
+            // Route pour toutes les sous-routes `/api/products/:path*`
+            {
+                source: '/api/products/:path*',
+                destination: 'https://la-juriste-proxy-ec8f63ca9ca5.herokuapp.com/api/products/:path*',
             },
         ];
     },
