2573436d6e4fb7eaaa4c31a84d1a6ae301214dac
jean-mulet <leo0609leo@gmail.com>
2025-01-08 13:35:57 +0100

    fix(): test

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
     async rewrites() {
         return [
             {
-                source: '/api/product/:id/pdf',
+                source: '/api/product/:path*/pdf',
                 destination: 'https://la-juriste-proxy-ec8f63ca9ca5.herokuapp.com/api/products/:path*',
             },
         ];
