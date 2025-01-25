5df10e2743184b59b154f73a9d0f662e38ec8c44
jean-mulet <leo0609leo@gmail.com>
2025-01-25 12:45:07 +0100

    fix(): push google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,43 @@
-import type { Metadata } from 'next';
 import React from "react";
 import '@/styles/globals.css';
 import Footer from "@/components/Footer";
 import ClientNavbar from "@/components/ClientNavBar";
 import CookiePopupManager from "@/components/utils/cookies/CookiePopupManager";
 import CleanUpOrders from "@lib/OrderLib/component/CleanupOrder";
-import ClientProviders from './(provider)/ClientProviders';   
+import ClientProviders from './(provider)/ClientProviders';
+import Script from "next/script";
 
-export const metadata: Metadata = {
-  title: "La juriste indépendante",
-  description: "Fleo web base pour tous les prochains sites Fleo",
-  icons: {
-    icon: "/favicon.ico",
-  },
-};
-
-
-export default function RootLayout({ children }: { children: React.ReactNode; }) {
-  return (
-    <html lang="fr">
-      <body className={`bodyGe`}>
+export default function RootLayout({children}: { children: React.ReactNode; }) {
+    return (
+        <html lang="fr">
+        <head>
+            <link rel="icon" href="/favicon.ico"/>
+            <title>La juriste indépendante</title>
+            <Script
+                id="google-tag-manager"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WJ5KLS9X');`,
+                }}
+            />
+        </head>
+        <body className={`bodyGe`}>
+        <noscript>
+            <iframe
+                src="https://www.googletagmanager.com/ns.html?id=GTM-WJ5KLS9X"
+                height="0"
+                width="0"
+                style={{ display: 'none', visibility: 'hidden' }}
+            ></iframe>
+        </noscript>
         <ClientProviders>
-          <CleanUpOrders />
-          <ClientNavbar />
-          {children}
-          <Footer />
-          <CookiePopupManager />
+            <CleanUpOrders/>
+            <ClientNavbar/>
+            {children}
+            <Footer/>
+            <CookiePopupManager/>
         </ClientProviders>
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
