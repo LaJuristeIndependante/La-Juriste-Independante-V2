80c77bc639e1462f9965aa3a10f2529c5c29798e
jean-mulet <leo0609leo@gmail.com>
2025-01-25 12:47:50 +0100

    fix(): push google adsence

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,10 @@ export default function RootLayout({children}: { children: React.ReactNode; }) {
                     __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WJ5KLS9X');`,
                 }}
             />
+            <Script
+                async
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7144018782963885"
+                crossOrigin="anonymous"></Script>
         </head>
         <body className={`bodyGe`}>
         <noscript>
