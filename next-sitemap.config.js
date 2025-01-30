317ece4f24ca3bd9d546c475d191b44897761570
jean-mulet <leo0609leo@gmail.com>
2025-01-30 15:30:09 +0100

    fix(): test changement

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,16 +1,4 @@
-const config = {
+module.exports = {
     siteUrl: "https://www.lajuristeindependante.com",
     generateRobotsTxt: true,
-    sitemapSize: 5000,
-    changefreq: "daily",
-    priority: 0.7,
-    exclude: ["/admin", "/dashboard"],
-    robotsTxtOptions: {
-        policies: [
-            { userAgent: "*", allow: "/" },
-            { userAgent: "*", disallow: ["/admin", "/dashboard"] },
-        ],
-    },
 };
-
-module.exports = config;
