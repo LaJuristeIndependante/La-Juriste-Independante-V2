4be7f28e56a381542ede6c46a0d3c4cc50d96ace
jean-mulet <leo0609leo@gmail.com>
2025-01-30 15:00:36 +0100

    fix(): update robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,17 @@
-module.exports = {
+/** @type {import('next-sitemap').IConfig} */
+const config = {
     siteUrl: "https://www.lajuristeindependante.com",
     generateRobotsTxt: true,
+    sitemapSize: 5000,
+    changefreq: "daily",
+    priority: 0.7,
+    exclude: ["/admin", "/dashboard"],
+    robotsTxtOptions: {
+        policies: [
+            { userAgent: "*", allow: "/" },
+            { userAgent: "*", disallow: ["/admin", "/dashboard"] },
+        ],
+    },
 };
+
+module.exports = config;
