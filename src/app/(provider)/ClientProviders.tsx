e65a25cee97136f297f8b48b188f66d99cfa0eef
jean-mulet <leo0609leo@gmail.com>
2025-01-30 15:24:49 +0100

    fix(): test changement

--- src/app/(provider)/ClientProviders.tsx
+++ src/app/(provider)/ClientProviders.tsx
@@ -11,11 +11,9 @@ const queryClient = new QueryClient();
 export default function ClientProviders({children}: { children: React.ReactNode }) {
     return (
         <QueryClientProvider client={queryClient}>
-            <AuthProvider>
-                <Provider>
-                    {children}
-                </Provider>
-            </AuthProvider>
+            <Provider>
+                {children}
+            </Provider>
         </QueryClientProvider>
     );
 }
